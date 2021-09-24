1cafc1f1afb8c77062a6c3ce3870745bce25d8a8
SnO2WMaN <me@sno2wman.net>
2021-09-24 16:46:22 +0900

    useViewer

--- src/libs/useViewer.ts
+++ src/libs/useViewer.ts
@@ -1,4 +1,8 @@
 import gql from 'graphql-tag';
+import {useEffect} from 'react';
+import {atom, useRecoilValue, useSetRecoilState} from 'recoil';
+
+import {useGetViewerQuery} from './useViewer.codegen';
 
 const UseViewerQuery = gql`
   query GetViewer {
@@ -9,3 +13,34 @@ const UseViewerQuery = gql`
     }
   }
 `;
+
+export type Viewer = {
+  id: string;
+  alias: string;
+  displayName: string;
+};
+
+export const viewerState = atom<undefined | null | Viewer>({
+  key: 'viewer',
+  default: undefined,
+});
+
+export const useViewer = () => {
+  const viewer = useRecoilValue(viewerState);
+  const setViewer = useSetRecoilState(viewerState);
+
+  const [result] = useGetViewerQuery();
+  const {data, fetching} = result;
+
+  useEffect(() => {
+    if (!fetching && data?.viewer)
+      setViewer({
+        id: data.viewer.id,
+        alias: data.viewer.alias,
+        displayName: data.viewer.displayName,
+      });
+    else if (!fetching && !data?.viewer) setViewer(null);
+  }, [data, fetching, setViewer]);
+
+  return viewer;
+};
