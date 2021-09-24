fbe2fff86e47c9ccc1c99e3c7dde789e3a0b0d45
SnO2WMaN <me@sno2wman.net>
2021-09-24 14:01:23 +0900

    pages

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -3,5 +3,7 @@
 declare namespace NodeJS {
   interface ProcessEnv extends NodeJS.ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
+
+    readonly NEXT_PUBLIC_GRAPHQL_API_ENDPOINT: string;
   }
 }
