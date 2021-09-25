c67d6ed769945e6e4b6840ddc6ea8671e4c3bf58
SnO2WMaN <me@sno2wman.net>
2021-09-25 10:54:17 +0900

    handler wip

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,34 @@
-import {GraphQLHandler} from 'msw';
+import {graphql, GraphQLHandler} from 'msw';
+import faker from 'faker';
 
-const devMocks: GraphQLHandler[] = [];
+import {GetViewerQuery, GetViewerQueryVariables} from './codegen';
+
+const devMocks: GraphQLHandler[] = [
+  graphql.query<GetViewerQuery, GetViewerQueryVariables>(
+    'GetViewer',
+    (req, res, ctx) => {
+      if (req.headers.get('Authorization'))
+        return res(
+          ctx.data({
+            __typename: 'Query',
+            viewer: {
+              __typename: 'User',
+              id: faker.datatype.uuid(),
+              alias: faker.hacker.ingverb(),
+              displayName: faker.name.firstName(),
+            },
+          }),
+        );
+      else
+        return res(
+          ctx.data({
+            __typename: 'Query',
+            viewer: null,
+          }),
+        );
+    },
+  ),
+];
 
 export const handlers =
   // eslint-disable-next-line no-process-env
