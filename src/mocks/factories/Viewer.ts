95de7bfb058b24819852029722136e7b02cb2c16
SnO2WMaN <me@sno2wman.net>
2021-10-08 16:42:33 +0900

    fx typecheck

--- src/mocks/factories/Viewer.ts
+++ src/mocks/factories/Viewer.ts
@@ -1,18 +1,19 @@
-import {GetViewerQuery} from '../codegen';
+import {FetchViewerQuery} from '../codegen';
 
-import {alias, displayName, id} from './common';
+import {alias, avatar, displayName, id} from './common';
 
-export const factoryUnauthorizedViewer = (): GetViewerQuery => ({
+export const factoryUnauthorizedViewer = (): FetchViewerQuery => ({
   __typename: 'Query',
   viewer: null,
 });
 
-export const factoryAuthorizedViewer = (): GetViewerQuery => ({
+export const factoryAuthorizedViewer = (): FetchViewerQuery => ({
   __typename: 'Query',
   viewer: {
     __typename: 'User',
     id: id(),
     alias: alias(),
     displayName: displayName(),
+    avatar: avatar(),
   },
 });
